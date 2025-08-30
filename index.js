b1e6aba7158484ca1d242798758c6ee9c04c9329
Marqquez45 <marqquez5622@gmail.com>
2025-08-30 07:36:46 -0500

    Animacion al hexagono

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const h = window.innerHeight;
 
 //WebGLRenderer es el motor que dibuja graficos 3D en el navegador
 //antialias: true -> subiza os bordes de los obetos
-const renderer = new THREE.WebGL3DRenderer({antialias: true});
+const renderer = new THREE.WebGLRenderer({antialias: true});
 
 
 //Ajustamos el tamnio del area de dibujo al ancho y alto de la ventana
@@ -23,21 +23,27 @@ const aspect = w / h;
 const near = 0.1;
 const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+camera.position.z = 5;
 
 
 const scene = new THREE.Scene();
 
-const geo = new THREE.IcosahedronGeometry(1.0, 2);
-const mat = new THREE.MeshPhongMaterial({
-    color: 0x0000ff
+const geo = new THREE.IcosahedronGeometry(1.0, 1);
+const mat = new THREE.MeshBasicMaterial({
+    color: 0xccff
 });
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
 
+let t = 0; // contador de tiempo
+function animate() {
+    requestAnimationFrame(animate);
+    t += 0.01; // avanza en cada frame
+    mesh.scale.setScalar(Math.cos(t) + 1.0); // escala oscila entre 0.5 y 2.5
+    renderer.render(scene, camera);
+}
 
-
-
+animate();
 
 //scene -> que cosas debe dibujar
 //camera -> desde donde mirar esos objetos
